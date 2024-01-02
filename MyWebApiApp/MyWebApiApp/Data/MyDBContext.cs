6364dc43b0ead2f271eae6dfb33fa7d2115f146b
CaoSang92 <CaoSang92@gmail.com>
2024-01-02 15:28:33 +0900

    DayTwo-EFCore

--- MyWebApiApp/MyWebApiApp/Data/MyDBContext.cs
+++ MyWebApiApp/MyWebApiApp/Data/MyDBContext.cs
@@ -23,6 +23,23 @@ namespace MyWebApiApp.Data
                 e.ToTable("Order");
                 e.HasKey(ord => ord.OrderId);
                 e.Property(ord => ord.OrderDate).HasDefaultValue(DateTime.Now);
+                e.Property(ord => ord.Receiver).IsRequired().HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<OrderDetail>(e =>
+            {
+                e.ToTable("OrderDetail");
+                e.HasKey(ord => new { ord.OrderId, ord.ProducId});
+                e.HasOne(e => e.Order)
+                .WithMany(e => e.OrderDetails)
+                .HasForeignKey(e => e.OrderId)
+                .HasConstraintName("FK_OrderDetail_Order");
+
+                e.HasOne(e => e.Product)
+                .WithMany(e => e.OrderDetails)
+                .HasForeignKey(e => e.ProducId)
+                .HasConstraintName("FK_OrderDetail_Product");
+
             });
         }
     }
---
+++ MyWebApiApp/MyWebApiApp/Migrations/20240102062104_AddOrderSchema.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyWebApiApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    OrderId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2024, 1, 2, 15, 21, 4, 409, DateTimeKind.Local).AddTicks(3099)),
+                    ShipDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    OrderStatus = table.Column<int>(type: "int", nullable: false),
+                    Receiver = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ShipPlace = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TelNumer = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.OrderId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetail",
+                columns: table => new
+                {
+                    OrderId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProducId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Discount = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetail", x => new { x.OrderId, x.ProducId });
+                    table.ForeignKey(
+                        name: "FK_OrderDetail_Order",
+                        column: x => x.OrderId,
+                        principalTable: "Order",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetail_Product",
+                        column: x => x.ProducId,
+                        principalTable: "M_Product",
+                        principalColumn: "ProducId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetail_ProducId",
+                table: "OrderDetail",
+                column: "ProducId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetail");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+        }
+    }
+}
